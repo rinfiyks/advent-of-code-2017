82b6dd5b1d1c6190402951ef1cca41194aa43640
rinfiyks <tom.gould.92@gmail.com>
2017-12-10 12:38:02 +0000

    Clean up duplicated code

--- scala/advent-of-code/src/main/scala/puzzles/Day9.scala
+++ scala/advent-of-code/src/main/scala/puzzles/Day9.scala
@@ -9,41 +9,30 @@ object Day9 extends App {
   println(solve2(input))
 
   def solve1(input: String): Int = {
-    input.foldLeft[(List[Symbol], Symbol)]((List.empty[Symbol], Bracket(0))) {
-      case ((symbols, Bracket(s)), c) =>
-        c match {
-          case '}' => (Bracket(s) +: symbols, Bracket(s - 1))
-          case '{' => (symbols, Bracket(s + 1))
-          case '<' => (symbols, Garbage(s))
-          case _ => (symbols, Bracket(s))
-        }
-      case ((symbols, Garbage(s)), c) =>
-        c match {
-          case '>' => (symbols, Bracket(s))
-          case '!' => (symbols, GarbageIgnore(s))
-          case _ => (symbols, Garbage(s))
-        }
-      case ((symbols, GarbageIgnore(s)), _) => (symbols, Garbage(s))
-    }._1.collect { case s: Bracket => s }.map(_.score).sum
+    parse(input)._1.collect { case s: Bracket => s }.map(_.score).sum
   }
 
   def solve2(input: String): Int = {
-    input.foldLeft[(Int, Symbol)]((0, Bracket(0))) {
-      case ((count, Bracket(s)), c) =>
+    parse(input)._2
+  }
+
+  private def parse(input: String) = {
+    input.foldLeft[(List[Symbol], Int, Symbol)]((List.empty[Symbol], 0, Bracket(0))) {
+      case ((symbols, count, Bracket(s)), c) =>
         c match {
-          case '}' => (count, Bracket(s - 1))
-          case '{' => (count, Bracket(s + 1))
-          case '<' => (count, Garbage(s))
-          case _ => (count, Bracket(s))
+          case '}' => (Bracket(s) +: symbols, count, Bracket(s - 1))
+          case '{' => (symbols, count, Bracket(s + 1))
+          case '<' => (symbols, count, Garbage(s))
+          case _ => (symbols, count, Bracket(s))
         }
-      case ((count, Garbage(s)), c) =>
+      case ((symbols, count, Garbage(s)), c) =>
         c match {
-          case '>' => (count, Bracket(s))
-          case '!' => (count, GarbageIgnore(s))
-          case _ => (count + 1, Garbage(s))
+          case '>' => (symbols, count, Bracket(s))
+          case '!' => (symbols, count, GarbageIgnore(s))
+          case _ => (symbols, count + 1, Garbage(s))
         }
-      case ((count, GarbageIgnore(s)), _) => (count, Garbage(s))
-    }._1
+      case ((symbols, count, GarbageIgnore(s)), _) => (symbols, count, Garbage(s))
+    }
   }
 
 }
